c985b0eb716b68e03441462e5e3fccc35a110edb
Dmitry Sharov <dsharov@griddynamics.com>
2017-02-06 18:08:01 +0300

    Added button control. normalized colors.

--- src/app/shared/components/popup/popup.component.ts
+++ src/app/shared/components/popup/popup.component.ts
@@ -3,10 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
     selector: 'app-popup',
     templateUrl: './popup.component.html',
-    styleUrls: [
-        './popup.component.scss',
-        './button.component.scss',
-    ],
+    styleUrls: ['./popup.component.scss'],
 })
 export class PopupComponent {
     @Input() public caption: string;
