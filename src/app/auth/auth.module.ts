6dc69d4152863c71915294ae7a036c5f04da75ee
Dmitry Sharov <dsharov@griddynamics.com>
2017-02-10 16:19:37 +0300

    Added subscriptions tracker service

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthActions } from './auth.actions';
-import { AuthService } from './auth.service';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './login/login.component';
 import { UserMenuComponent } from './user-menu/user-menu.component';
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,9 @@ import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CloseSubscriptionsComponent } from './helpers/clean-subscriptions.component';
 import { throwIfAlreadyLoaded } from './module-import-guard';
+import { SubscriptionTrackerService } from './subscription-tracker.service';
 
 @NgModule({
     imports: [
@@ -11,9 +13,9 @@ import { throwIfAlreadyLoaded } from './module-import-guard';
         HttpModule,
         BrowserModule,
     ],
-    declarations: [],
-    providers: [],
-    exports: [],
+    declarations: [CloseSubscriptionsComponent],
+    providers: [SubscriptionTrackerService],
+    entryComponents: [ CloseSubscriptionsComponent ],
 })
 export class CoreModule {
     constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
